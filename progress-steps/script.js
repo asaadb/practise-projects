db32f3e7a67022fdb7113c243e48cb6cda9fecf6
Asaad Budayr <asad_289@hotmail.com>
2022-08-16 22:57:40 +0300

    refactor: Refactor JS

--- progress-steps/script.js
+++ progress-steps/script.js
@@ -3,30 +3,38 @@ const prevBtn = document.querySelector('#prev');
 
 // This keeps track of the steps taken
 let steps = 1;
+const maxSteps = 4;
 
 nextBtn.addEventListener('click', function () {
     steps++;
     const progress = document.querySelector(`.step-container:nth-of-type(${steps})`)
     progress.classList.add('active')
-    if(steps > 1) {
-       prevBtn.disabled = false;
-       prevBtn.classList.remove('disabled')
-     }
-    if (steps === 4){
-        this.disabled = true;
-        this.classList.add('disabled')
+    if (steps > 1) {
+        enableBtn(prevBtn);
+    }
+    if (steps === maxSteps){
+        disableBtn(this);
     }
 })
+
 prevBtn.addEventListener('click', function () {
     steps--;
     const progress = document.querySelector(`.step-container:nth-of-type(${steps + 1})`)
     progress.classList.remove('active')
-    if(steps < 4) {
-        nextBtn.disabled = false;
-        nextBtn.classList.remove('disabled')
+    if ( steps < maxSteps) {
+        enableBtn(nextBtn);
     }
     if (steps === 1){
-        this.disabled = true;
-        this.classList.add('disabled')
+        disableBtn(this);
     }
-});
+})
+
+function disableBtn(btn) {
+    btn.disabled = true;
+    btn.classList.add('disabled');
+}
+
+function enableBtn (btn) {
+    btn.disabled = false;
+    btn.classList.remove('disabled');
+}
