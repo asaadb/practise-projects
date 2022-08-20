f6984585930bed0ac2fd8b578caa97240ce27b7f
Asaad Budayr <asad_289@hotmail.com>
2022-08-20 11:51:04 +0300

    feat: Add rotation animation

--- rotating-nav-animation/script.js
+++ rotating-nav-animation/script.js
@@ -1,11 +1,18 @@
 const open = document.querySelector(".open");
 const close = document.querySelector(".close");
+const article = document.querySelector(".article-container")
+const menuIcons = document.querySelector(".menuIcons-container");
 
 open.addEventListener("click", function () {
     this.classList.remove('active');
     close.classList.add('active');
+    article.classList.add('active');
+    menuIcons.classList.add("active");
+    
 })
 close.addEventListener("click", function () {
     this.classList.remove('active');
     open.classList.add('active');
+    article.classList.remove('active');
+    menuIcons.classList.remove("active");
 })
