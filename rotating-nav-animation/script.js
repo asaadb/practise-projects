5147523ec195c34737da6d8f8f6d4adba3a4b853
Asaad Budayr <asad_289@hotmail.com>
2022-08-22 11:22:12 +0300

    refactor: Refactor code

--- rotating-nav-animation/script.js
+++ rotating-nav-animation/script.js
@@ -1,22 +1,11 @@
 const open = document.querySelector(".open");
 const close = document.querySelector(".close");
-const article = document.querySelector(".article-container")
-const menuIcons = document.querySelector(".menuIcons-container");
-const nav = document.querySelector(".nav");
-
+const container = document.querySelector(".main-container");
 
 open.addEventListener("click", function () {
-    this.classList.remove('active');
-    close.classList.add('active');
-    article.classList.add('active');
-    menuIcons.classList.add("active");
-    nav.classList.add("active");
+    container.classList.add('active');
     
 })
 close.addEventListener("click", function () {
-    this.classList.remove('active');
-    open.classList.add('active');
-    article.classList.remove('active');
-    menuIcons.classList.remove("active");
-    nav.classList.remove("active");
+    container.classList.remove('active');
 })
