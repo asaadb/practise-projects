33c509d7817190cf7081d623ff995954f6fbbd8c
Asaad Budayr <asad_289@hotmail.com>
2022-08-24 22:45:55 +0300

    feat: Add blurring and opacity effects

--- blurry-loading/script.js
+++ blurry-loading/script.js
@@ -1,14 +1,23 @@
 const loading = document.querySelector(".loading");
+const imgContainer = document.querySelector(".img-container");
+
+loadingEffect();
 
 function loadingEffect () {
     let num = 0;
+    let opacity = 1;
+    let blur = 1;
     let intervalId = setInterval(() => {
         loading.textContent = `${num}%`;
+        loading.style.opacity = `${opacity}`;
+        imgContainer.style.filter = `blur(${blur}rem)`;
         num++;
+        opacity -= 0.01;
+        blur -= 0.01;
+        console.log(opacity)
+        console.log(num)
         if (num > 100) {
-            clearInterval(intervalId)
+            clearInterval(intervalId);
         }
-    }, 28)
+    }, 30)
 }
-
-loadingEffect();
