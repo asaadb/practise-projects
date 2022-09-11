128c3f25aab672e74c7d52865cc564db1965667c
Asaad Budayr <asad_289@hotmail.com>
2022-09-11 18:56:08 +0300

    feat(dad-jokes): Add loader animation

--- dad-jokes/script.js
+++ dad-jokes/script.js
@@ -1,12 +1,15 @@
 const joke = document.getElementById('joke');
 const jokeBtn = document.querySelector('.joke-btn');
+const loader = document.querySelector('.loader');
 
-//fetch the first joke
+// fetch the first joke
 fetchJoke();
 
 jokeBtn.addEventListener('click', fetchJoke);
 
 function fetchJoke () {
+    loader.style.display = 'block';
+    joke.textContent = '';
     const config = { headers: { Accept:"application/json" } }
     fetch("https://icanhazdadjoke.com/", config)
     .then(res => {
@@ -16,10 +19,11 @@ function fetchJoke () {
         renderJokes(data);
     })
     .catch ((err) => {
-            console.log("ERROR!!", err);
+        console.log("ERROR!!", err);
     })
 }
 
 function renderJokes(data) {
+    loader.style.display = 'none'
     joke.textContent = data.joke;
 }
